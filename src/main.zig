0c6d25e10e6954d9a96869985382ba915cafe29e
Rik <rik.de.kort@hey.com>
2021-12-01 22:30:45 +0100

    progress!

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
 
 const MTError = error{ Overrun, Interesting };
@@ -9,7 +10,7 @@ const TestCase = struct {
     history: ArrayList(u8),
     index: usize,
 
-    pub fn init(alloc: *std.mem.Allocator) Self {
+    pub fn init(alloc: *Allocator) Self {
         return Self{ .rng = std.rand.Isaac64.init(@intCast(u64, std.time.milliTimestamp())), .history = ArrayList(u8).init(alloc), .index = 0 };
     }
 
@@ -58,14 +59,11 @@ const InterestTest = fn (tc: *TestCase) MTError!bool;
 const MAX_RUNS = 1000;
 
 /// Helper to create a test case with history given by to_check and apply the InterestTest on it.
-fn interesting_for_slice(to_check: []u8, itest: InterestTest) bool {
-    var arena: std.heap.ArenaAllocator = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-
-    var list = ArrayList(u8).init(&arena.allocator);
-    // std.debug.print("Trying to append... {any}\n", .{to_check});
+fn interesting_for_slice(alloc: *Allocator, to_check: []u8, itest: InterestTest) bool {
+    var list = ArrayList(u8).init(alloc);
     list.appendSlice(to_check) catch return false;
-    // std.debug.print("Append succesful!", .{});
     var tc: TestCase = TestCase.for_history(list);
+    defer tc.deinit();
     return itest(&tc) catch false;
 }
 
@@ -78,17 +76,32 @@ fn interesting_if_ge_10(tc: *TestCase) MTError!bool {
 
 test "interesting_for_slice" {
     var buf: [1]u8 = .{1};
-    try std.testing.expect(!interesting_for_slice(buf[0..1], interesting_if_ge_10));
+    try std.testing.expect(!interesting_for_slice(std.testing.allocator, buf[0..1], interesting_if_ge_10));
     buf = .{10};
-    try std.testing.expect(interesting_for_slice(buf[0..1], interesting_if_ge_10));
+    try std.testing.expect(interesting_for_slice(std.testing.allocator, buf[0..1], interesting_if_ge_10));
     buf = .{100};
-    try std.testing.expect(interesting_for_slice(buf[0..1], interesting_if_ge_10));
+    try std.testing.expect(interesting_for_slice(std.testing.allocator, buf[0..1], interesting_if_ge_10));
 }
 
-fn shrink_reduce(history: []u8, interesting: InterestTest) []u8 {
-    if (history.len == 0) return history;
-    while (interesting_for_slice(history, interesting) and history[0] > 0) : (history[0] -= 1) {}
-    return history;
+fn shrink_reduce(alloc: *Allocator, attempt: []u8, interesting: InterestTest) anyerror![]u8 {
+    if (attempt.len == 0) return attempt;
+    // std.debug.print("shrinking attempt {any}\n", .{attempt});
+    var known_good = try std.mem.dupe(alloc, u8, attempt);
+    var i: usize = attempt.len - 1;
+    while (i >= 0) : (i -= 1) {
+        // std.debug.print("Reducing {} for {any}\n", .{ i, attempt });
+        std.mem.copy(u8, attempt, known_good);
+        while (interesting_for_slice(alloc, attempt, interesting)) {
+            std.mem.copy(u8, known_good, attempt);
+
+            if (attempt[i] == 0) break;
+            attempt[i] -= 1;
+        }
+        if (i == 0) break;
+    }
+    // std.debug.print("I need a drink {}\n", .{interesting_for_slice(known_good, interesting)});
+    // std.debug.print("attempt: {any}, known_good: {any}\n", .{ attempt, known_good });
+    return known_good;
 }
 
 pub fn shrink(history: []u8, interesting: InterestTest) []u8 {
@@ -96,7 +109,7 @@ pub fn shrink(history: []u8, interesting: InterestTest) []u8 {
     return history;
 }
 
-pub fn run_test(alloc: *std.mem.Allocator, interesting: InterestTest) ?[]u8 {
+pub fn run_test(alloc: *Allocator, interesting: InterestTest) anyerror!?[]u8 {
     var tc = TestCase.init(alloc);
     defer tc.deinit();
 
@@ -108,19 +121,27 @@ pub fn run_test(alloc: *std.mem.Allocator, interesting: InterestTest) ?[]u8 {
         is_interesting = interesting(&tc) catch false;
     }
     if (is_interesting) {
-        const shrunk: []u8 = shrink_reduce(tc.history.items, interesting);
-        std.debug.print("Got an interersting test case with choices {any}.\n", .{shrunk});
+        const shrunk: []u8 = try shrink_reduce(alloc, tc.history.items, interesting);
+        std.debug.print("Got an interesting test case with choices {any}.\n", .{shrunk});
         return shrunk;
     } else {
         return null;
     }
 }
 
+pub fn interesting_if_gt_10(x: []u8) bool {
+    return x[0] > 10;
+}
+
 pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
-    var buf: [1]u8 = .{10};
-    const result: bool = interesting_for_slice(buf[0..1], interesting_if_ge_10);
-    std.debug.print("{}", .{result});
+    var i: usize = 0;
+    while (i < 10) : (i += 1) {
+        var j: usize = 0;
+        while (j < 10) : (j += 1) {
+            if (j > 3) break;
+        }
+        std.debug.print("i={}\n", .{i});
+    }
 }
 
 fn always_interesting(tc: *TestCase) MTError!bool {
@@ -129,7 +150,7 @@ fn always_interesting(tc: *TestCase) MTError!bool {
 }
 
 test "Always interesting" {
-    try std.testing.expect(null != run_test(std.testing.allocator, always_interesting));
+    try std.testing.expect(null != try run_test(std.testing.allocator, always_interesting));
 }
 
 fn never_interesting(tc: *TestCase) MTError!bool {
@@ -138,7 +159,7 @@ fn never_interesting(tc: *TestCase) MTError!bool {
 }
 
 test "Never interesting" {
-    try std.testing.expect(null == run_test(std.testing.allocator, never_interesting));
+    try std.testing.expect(null == try run_test(std.testing.allocator, never_interesting));
 }
 
 fn rarely_interesting(tc: *TestCase) MTError!bool {
@@ -148,7 +169,28 @@ fn rarely_interesting(tc: *TestCase) MTError!bool {
 }
 
 test "Hard to find" {
-    const expected: [1]u8 = .{9};
-    const result = run_test(std.testing.allocator, rarely_interesting) orelse return error.TestExpectedEqual;
+    const expected: [1]u8 = .{0};
+    const result = (try run_test(std.testing.allocator, rarely_interesting)) orelse return error.TestExpectedEqual;
+    defer std.testing.allocator.free(result);
+    try std.testing.expectEqualSlices(u8, expected[0..expected.len], result);
+}
+
+test "Reduce shrinking" {
+    const expected: [1]u8 = .{10};
+    const result = (try run_test(std.testing.allocator, interesting_if_ge_10)) orelse return error.TestExpectedEqual;
+    defer std.testing.allocator.free(result);
+    try std.testing.expectEqualSlices(u8, expected[0..expected.len], result);
+}
+
+fn interesting_if_both_ge_10(tc: *TestCase) MTError!bool {
+    var buf: [2]u8 = undefined;
+    try tc.get_bytes(&buf);
+    return buf[0] >= 10 and buf[1] >= 10;
+}
+
+test "Reduce shrinking in other position" {
+    const expected: [2]u8 = .{ 10, 10 };
+    const result = (try run_test(std.testing.allocator, interesting_if_both_ge_10)) orelse return error.TestExpectedEqual;
+    defer std.testing.allocator.free(result);
     try std.testing.expectEqualSlices(u8, expected[0..expected.len], result);
 }
